6173dcfc2f0cca3d7f0d53230872370cf1aa1d55
Nhat Hoang <103094181+NhatHoanggg@users.noreply.github.com>
2024-12-19 21:13:33 +0700

    feat/ swagger ui

---
+++ UNIME/src/main/java/PBL6/example/UNIME/configuration/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package PBL6.example.UNIME.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("UNIME API Documentation")
+                        .description("API documentation for the UNIME application.")
+                        .version("1.0"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Input your Bearer token in the format: `Bearer <token>`")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
