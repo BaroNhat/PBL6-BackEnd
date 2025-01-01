77a27be351a34ecb8ce4028929d04a848cf653d5
phuongnhung <2706nhunghoang@gmail.com>
2025-01-01 16:19:46 +0700

    fix tokenTime

--- UNIME/src/main/java/PBL6/example/UNIME/service/AppointmentServiceImpl.java
+++ UNIME/src/main/java/PBL6/example/UNIME/service/AppointmentServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.IsoFields;
 import java.time.temporal.WeekFields;
 import java.util.List;
 import java.util.Locale;
